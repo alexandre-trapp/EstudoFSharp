7e5a87866b9067b13b5db66a4bf64e617b33d782
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-09-15 18:06:57 -0300

    Add project files.

---
+++ ExerciseList02.fsx
@@ -0,0 +1,6 @@
+﻿module ExerciseList02
+
+let mutable a = 10
+a <- 20
+
+printfn "%d value of a" a
---
+++ ExerciseSquare01.fsx
@@ -0,0 +1,4 @@
+﻿module HelloSquare
+
+let square x = x * x
+printfn "%d squared is: %d!" 12 (square 12)
