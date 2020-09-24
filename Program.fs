bb614f48ce306b88fc6d343577b095648a51b1fe
Alexandre <alexandre.trapp@hotmail.com>
2020-09-24 01:28:06 -0300

    add functions examples F#

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Tour.Functions
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    printfn "%d" BasicFunctions.result1
+    0 // return an integer exit code
