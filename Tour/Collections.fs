3b6b57881e996f1ec8781eb1a1079702361e2caa
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-09-30 23:12:57 -0300

    add collections

--- Tour/Collections.fs
+++ Tour/Collections.fs
@@ -96,3 +96,39 @@ module Arrays =
         |> Array.sumBy (fun x -> x.Length)
 
     printfn "The sum of the lengths of the words in Array 2 is: %d" sumOfLengthsOfWords
+
+module Sequences =
+
+    /// This is the empty sequence.
+    let seq1 = Seq.empty
+
+    /// This a sequence of values.
+    let seq2 = seq { yield "hello"; yield "world"; yield "and"; yield "hello"; yield "world"; yield "again" }
+
+    /// This is an on-demand sequence from 1 to 1000.
+    let numbersSeq = seq { 1 .. 1000 }
+
+    /// This is a sequence producing the words "hello" and "world"
+    let seq3 =
+        seq { for word in seq2 do
+                    if word.Contains("l") then
+                        yield word }
+
+    /// This sequence producing the even numbers up to 2000.
+    let evenNumbers = Seq.init 1001 (fun n -> n * 2)
+
+    let rnd = System.Random()
+
+    /// This is an infinite sequence which is a random walk.
+    /// This example uses yield! to return each element of a subsequence.
+    let rec randomWalk x =
+        seq { yield x
+              yield! randomWalk (x + rnd.NextDouble() - 0.5) }
+
+    /// This example shows the first 100 elements of the random walk.
+    let first100ValuesOfRandomWalk = 
+        randomWalk 5.0
+        |> Seq.truncate 100
+        |> Seq.toList
+
+    printfn "First 100 elements of a random walk: %A" first100ValuesOfRandomWalk
