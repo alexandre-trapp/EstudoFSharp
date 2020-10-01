ce57f77cb6cfa5d9584207dcf34083d820ab8685
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-09-30 22:53:59 -0300

    add collections, primitives

--- Program.fs
+++ Program.fs
@@ -1,11 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
 open Tour.Functions
+open Tour.Collections
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
 
     BasicFunctions.result1
+
+    Lists.sumOfSquares
     0 // return an integer exit code
+
